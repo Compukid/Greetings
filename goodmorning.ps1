229b19aeec3c271f57c5286ab04e7c7e23f2a0ea
Eric Jacobowitz <compukid377@gmail.com>
2021-10-20 22:19:08 -0400

    First commit

---
+++ goodmorning.ps1
@@ -0,0 +1,6 @@
+﻿add-type -AssemblyName system.speech
+$speak = new-object System.Speech.Synthesis.SpeechSynthesizer
+$speak.speak('Good morning, Princeton and Mommy')
+$speak.speak('Please enjoy your class on the computer today')
+$speak.speak('I miss you, love Daddy')
+Disable-ScheduledTask -TaskName 'greetings' -TaskPath \home\
